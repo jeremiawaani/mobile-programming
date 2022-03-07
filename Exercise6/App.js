aee1d4519e5d94ab41a05c1a323b9edc78653a6b
Salomo Aray Mandagi <s21910316@student.unklab.ac.id>
2022-03-07 12:34:55 +0800

    Deklarasi state header dalam constructor, import library StyleSheet, membuat komponen Header dan styling header

--- Exercise6/App.js
+++ Exercise6/App.js
@@ -1,18 +1,47 @@
 import React, {Component} from 'react';
-import {Text, View, StatusBar} from 'react-native';
+import {Text, View, StatusBar, StyleSheet} from 'react-native';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      header: '',
+    };
   }
   render() {
     return (
       <View>
+        {/* StatusBar */}
         <StatusBar barStyle="light-content" backgroundColor="#0097A7" />
+
+        {/* Header */}
+        <View style={styles.header}>
+          <Text style={styles.headerTextTop}>Header</Text>
+          <Text style={styles.headerTextDown}>
+            {/* Disini akan menerima InputText tadi dan ditampilkan ke screen Header */}
+            {this.state.header}
+          </Text>
+        </View>
       </View>
     );
   }
 }
-
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#00BCD4',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  headerTextTop: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  headerTextDown: {
+    color: 'yellow',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
 export default App;
