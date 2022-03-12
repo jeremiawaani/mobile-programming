846d09ad35360895ae8696cb03422414282fea40
Erick <S21910250@student.unklab.ac.id>
2022-03-12 09:31:29 +0800

    Menambahkan fungsi useState Menambahkan component button Menghapus function Input Menambahkan function handleClick Menambahkan fungsi textInput untuk name Menambahkan text untuk menampilkan text yang di input ke monitor

--- exercise_7/App.js
+++ exercise_7/App.js
@@ -2,35 +2,59 @@ import React, {Component, useState} from 'react';
 import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState('E');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [city, setCity] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const handleClick = () => {
+    console.log(`Name: ${name}`);
+    console.log(`Email: ${email}`);
+    console.log(`Password: ${password}`);
+    console.log(`City: ${city}`);
+    console.log(`Phone: ${phone}`);
+  };
   return (
     <View
       style={{justifyContent: 'center', alignItems: 'center', marginTop: 60}}>
-      <Input text="Name" />
-      <Input text="Email" />
-      <Input text="Password" />
-      <Input text="City" />
-      <Input text="Phone" />
+      {/* TEXTINPUT NAME */}
+      <View style={{margin: 3}}>
+        <TextInput
+          style={{
+            borderColor: '#00BCD4',
+            borderWidth: 1,
+            width: 150,
+            height: 40,
+            fontSize: 16,
+            color: '#000',
+          }}
+          placeholderTextColor={'grey'}
+          placeholder="Name"
+          onChangeText={value => setName(value)}
+        />
+      </View>
+
+      <Text style={{fontSize: 16, color: '#000'}}>
+        Name : {name}
+        {'\n'}
+        Email : {email}
+        {'\n'}
+        Password: {password}
+        {'\n'}
+        City : {city}
+        {'\n'}
+        Phone : {phone}
+        {'\n'}
+      </Text>
+
+      <View style={{marginTop: 25}}>
+        <Button title="Press Me!" onPress={handleClick} />
+      </View>
     </View>
   );
 };
 
-const Input = ({text}) => {
-  return (
-    <View style={{margin: 3}}>
-      <TextInput
-        style={{
-          borderColor: '#00BCD4',
-          borderWidth: 1,
-          width: 150,
-          height: 40,
-          fontSize: 14,
-        }}
-        placeholderTextColor={'grey'}
-        placeholder={text}
-      />
-    </View>
-  );
-};
 export default App;
 
 const styles = StyleSheet.create({});
