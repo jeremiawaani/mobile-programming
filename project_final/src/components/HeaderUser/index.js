453f333146f1b1332c2e3998cecc7a33ee66f82f
Erick <S21910250@student.unklab.ac.id>
2022-04-25 19:15:01 +0800

    Menambahkan component HeaderUser

---
+++ project_final/src/components/HeaderUser/index.js
@@ -0,0 +1,34 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {IconBack, Profile} from '../../../assets';
+import Gap from '../Gap';
+
+const HeaderUser = ({title}) => {
+  return (
+    <View style={styles.container}>
+      <Gap width={29} />
+      <IconBack style={styles.iconBack} />
+      <Gap width={91} />
+      <Text style={styles.txt}>{title}</Text>
+      <Gap width={78} />
+      <Profile />
+      <Gap width={38} />
+    </View>
+  );
+};
+
+export default HeaderUser;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 94,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  txt: {
+    fontSize: 15,
+    color: '#000000',
+    fontFamily: 'Segoe UI Bold',
+  },
+});
