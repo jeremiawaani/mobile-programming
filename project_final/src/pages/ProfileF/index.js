f3e88422881ce53c2e454722ebd6ed6940d11a42
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2022-04-28 19:43:11 +0800

    add new page - Profile Freelancer

---
+++ project_final/src/pages/ProfileF/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProfileF = () => {
+  return (
+    <View>
+      <Text>ProfileF</Text>
+    </View>
+  )
+}
+
+export default ProfileF
+
+const styles = StyleSheet.create({})
