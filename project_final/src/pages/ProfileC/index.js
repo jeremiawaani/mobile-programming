dc98b8c3b1fea73f18b95ce2664205b3212d40e7
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2022-04-28 19:44:40 +0800

    Add new page - Profile Client

---
+++ project_final/src/pages/ProfileC/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProfileC = () => {
+  return (
+    <View>
+      <Text>ProfileC</Text>
+    </View>
+  )
+}
+
+export default ProfileC
+
+const styles = StyleSheet.create({})
