16bd65180db2102b6e146606458b05377e62ad6a
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2022-04-28 19:37:00 +0800

    adding the folder to index.js in components folder

--- project_final/src/components/index.js
+++ project_final/src/components/index.js
@@ -3,6 +3,6 @@ import Gap from './Gap';
 import HeaderUser  from './HeaderUser';
 import DetailsF_Bar from './DetailsF_Bar';
 import DetailsF_Header from './DetailsF_Bar';
+import ProfileF_Top from './ProfileF_Top';
 
-
-export {Footer, Gap, HeaderUser, DetailsF_Header, DetailsF_Bar};
+export {Footer, Gap, HeaderUser, DetailsF_Header, DetailsF_Bar, ProfileF_Top;
