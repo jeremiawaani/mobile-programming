fe3c5e12ac5fc419dd7dfe137a11b58bb373e95e
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2022-04-28 19:40:03 +0800

    Add new component ProfileF_Bottom

---
+++ project_final/src/components/ProfileF_Bottom/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProfileF_Bottom = () => {
+  return (
+    <View>
+      <Text>ProfileF_Bottom</Text>
+    </View>
+  )
+}
+
+export default ProfileF_Bottom
+
+const styles = StyleSheet.create({})
