c44dbc68155f18d92d0e342c883473af56c6498d
Jeremia Waani <s21910028@student.unklab.ac.id>
2022-04-28 11:39:15 +0800

    Mengganti penamaan folder yang akhiran U(User) menjadi F(Freelancer)

--- project_final/src/components/DetailsF_Bar/index.js
+++ project_final/src/components/DetailsF_Bar/index.js
@@ -1,14 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const DetailsU_Bar = () => {
+const DetailsF_Bar = () => {
   return (
     <View>
-      <Text>DetailsU_Bar</Text>
+      <Text>DetailsF_Bar</Text>
     </View>
   );
 };
 
-export default DetailsU_Bar;
+export default DetailsF_Bar;
 
 const styles = StyleSheet.create({});
