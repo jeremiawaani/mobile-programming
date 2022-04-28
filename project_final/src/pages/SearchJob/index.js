1f1b310ad4e28e9341ff86aa2c47f93ec63736b7
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2022-04-28 19:45:33 +0800

    Add new page - Search Job for Freelancer

---
+++ project_final/src/pages/SearchJob/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SearchJob = () => {
+  return (
+    <View>
+      <Text>SearchJob</Text>
+    </View>
+  )
+}
+
+export default SearchJob
+
+const styles = StyleSheet.create({})
