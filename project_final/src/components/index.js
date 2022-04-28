425f85b466f40928ba2001bf5d3c9ea171e00c69
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2022-04-28 19:41:24 +0800

    Adding the component to index.js in component folder to import and export

--- project_final/src/components/index.js
+++ project_final/src/components/index.js
@@ -4,5 +4,6 @@ import HeaderUser  from './HeaderUser';
 import DetailsF_Bar from './DetailsF_Bar';
 import DetailsF_Header from './DetailsF_Bar';
 import ProfileF_Top from './ProfileF_Top';
+import ProfileF_Bottom from './ProfileF_Bottom';
 
-export {Footer, Gap, HeaderUser, DetailsF_Header, DetailsF_Bar, ProfileF_Top;
+export {Footer, Gap, HeaderUser, DetailsF_Header, DetailsF_Bar, ProfileF_Top, ProfileF_Bottom;
