e9828cc7e80b35a637457895dc3c4479d480f4eb
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2022-05-01 20:37:25 +0800

    tambah gambar di ProfileF_Top component

--- project_final/src/components/ProfileF_Top/index.js
+++ project_final/src/components/ProfileF_Top/index.js
@@ -1,9 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Profile } from '../../../assets'
+import Gap from '../Gap'
 
 const ProfileF_Top = ({name='Erick Chang', status='Senior Designer'}) => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Profile height={70} width={70}/>
       
     </View>
   )
@@ -11,4 +14,13 @@ const ProfileF_Top = ({name='Erick Chang', status='Senior Designer'}) => {
 
 export default ProfileF_Top
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 20,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+})
