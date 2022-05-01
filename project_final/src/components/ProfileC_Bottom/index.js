02349ca8f0d2bbe556b14665339747d438fd3c6c
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2022-05-01 22:36:46 +0800

    tambah isi ProfileC_Top dan ProfileC_Bottom

--- project_final/src/components/ProfileC_Bottom/index.js
+++ project_final/src/components/ProfileC_Bottom/index.js
@@ -1,14 +1,70 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {Gap, Profile_Button} from '../../components'
+import { Profile, Pencil, Info, Achievement, Skills, IconBack } from '../../../assets'
 
 const ProfileC_Bottom = () => {
   return (
-    <View>
-      <Text>ProfileC_Bottom</Text>
+    <View style={styles.page}>
+      <Text style={{marginLeft: 50, marginTop: 17, fontFamily: 'Segoe UI', fontSize: 13}}>Dashboard</Text>
+      <Gap height={12} />
+      <View style={styles.container}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{ flex: 1}}>
+            <Profile_Button style={{ flex: 1}} width={40} title={<Info/>} color='#D2F5FD'></Profile_Button>
+          </View>
+          <View style={{flex:3, alignItems: 'flex-start'}}>
+            <Text style={{fontFamily: 'Segoe UI Bold', color: 'black', fontSize: 16}}>Company Profile</Text>
+          </View>
+          <View style={{alignItems:'flex-end', flex:1}}>
+            <IconBack style={{transform: [{ rotateY: '180deg'}], }} />
+          </View>
+        </View>
+        <Gap height={11} />
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{ flex: 1}}>
+            <Profile_Button style={{ flex: 1}} width={40} title={<Achievement/>} color='#D2F5FD'></Profile_Button>
+          </View>
+          <View style={{flex:3, alignItems: 'flex-start'}}>
+            <Text style={{fontFamily: 'Segoe UI Bold', color: 'black', fontSize: 16}}>Achievement</Text>
+          </View>
+          <View style={{alignItems:'flex-end', flex:1}}>
+            <IconBack style={{transform: [{ rotateY: '180deg'}], }} />
+          </View>
+        </View>
+        <Gap height={11} />
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{ flex: 1}}>
+            <Profile_Button style={{ flex: 1}} width={40} title={<Skills/>} color='#D2F5FD'></Profile_Button>
+          </View>
+          <View style={{flex:3, alignItems: 'flex-start'}}>
+            <Text style={{fontFamily: 'Segoe UI Bold', color: 'black', fontSize: 16}}>Skills</Text>
+          </View>
+          <View style={{alignItems:'flex-end', flex:1}}>
+            <IconBack style={{transform: [{ rotateY: '180deg'}], }} />
+          </View>
+        </View>
+        <Gap height={30} />
+        <Profile_Button width={270} title='Log Out' color='#D2F5FD'></Profile_Button>
+      </View>
     </View>
   )
 }
 
 export default ProfileC_Bottom
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page:{
+    paddingVertical: 20,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  container: {
+    marginLeft: 60,
+    marginRight: 45,
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+ 
+})
--- project_final/src/components/ProfileC_Top/index.js
+++ project_final/src/components/ProfileC_Top/index.js
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View  } from 'react-native'
 import React from 'react'
+import { Pencil, Profile } from '../../../assets'
+import {Gap, Profile_Button} from '../../components'
 
-const ProfileC_Top = () => {
+const ProfileC_Top = ({name='Salomo Mandagi', status='JCOs Director'}) => {
   return (
-    <View>
-      <Text>ProfileC_Top</Text>
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <Profile height={70} width={70}/>
+        <Gap width={23} />
+        <View>
+          <Text style={styles.textName}>{name}</Text>
+          <Text style={styles.statusName}>{status}</Text>
+        </View>
+        <Gap width={60} />
+        <Pencil />
+      </View>
+    <Text style={{marginLeft: 50, marginTop: 17, fontFamily: 'Segoe UI', fontSize: 13}}>My Status</Text>
+    <Gap height={10} />
+    <View style={styles.container}>
+      <Profile_Button title='Online' color='#E2D4FE'/>
+      <Gap width={9} />
+      <Profile_Button title='Offline' color='#D1FBEA'/>
+      <Gap width={9} />
+      <Profile_Button title='Looking for Service' color='#D2F5FD'/>
     </View>
+    </View>
+    
   )
 }
 
 export default ProfileC_Top
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page:{
+    paddingVertical: 20,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textName: {
+    fontFamily: 'Segoe UI Bold',
+    fontSize: 16, 
+    color: 'black',
+  },
+  statusName: {
+    fontFamily: 'Segoe UI'
+  },
+ 
+})
