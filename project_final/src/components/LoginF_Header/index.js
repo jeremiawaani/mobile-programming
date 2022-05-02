f82b980535f07f5e1293f5b39c6cbc3935eb429f
slmandagi <92778838+slmandagi@users.noreply.github.com>
2022-05-02 19:10:32 +0800

    Menambahkan komponen molekul Header pada page Login Freelance

---
+++ project_final/src/components/LoginF_Header/index.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { IconBack, Logo } from '../../../assets'
+import { Gap } from '../../atoms'
+
+const LoginF_Header = ({text}) => {
+  return (
+    <View style={styles.container}>
+        {/*Icon Back*/}
+        <View style={styles.iconBack}>
+          <IconBack />
+        </View>
+        {/*Logo dan Teks*/}
+        <View style={styles.logoWrapper}>
+          {/* <Gap height={20}/> */}
+          <Logo />
+          <Text style={styles.txt}>{text}
+          </Text>
+        </View>
+    </View>
+  )
+}
+
+export default LoginF_Header
+
+const styles = StyleSheet.create({
+    container:{
+        height: 234,
+    },
+    iconBack:
+    {
+        marginLeft: 29,
+        marginTop: 42
+    },
+    logoWrapper:
+    {
+        alignItems: 'center'
+    },
+    txt:
+    {
+        fontSize: 24,
+        color: '#8DEAFF',
+        fontFamily: 'Segoe UI Bold',
+    }
+
+})
