a978f025608a8ef6b33610ced5e2e0b1ebc454a8
slmandagi <92778838+slmandagi@users.noreply.github.com>
2022-05-02 19:01:30 +0800

    Menambahkan komponen TextInput pada page Login Freeelance

---
+++ project_final/src/components/LoginF_TextInput/index.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View, TextInput as TextInputRN } from 'react-native'
+import React from 'react'
+import Gap from '../Gap'
+
+const LoginF_TextInput = ({placeholder}) => {
+  return (
+    <View>
+      <TextInputRN style={styles.txtInput}>{placeholder}</TextInputRN>
+    </View>
+  )
+}
+
+export default LoginF_TextInput
+
+const styles = StyleSheet.create({
+
+    txtInput:{
+        borderColor: '#8DEAFF',
+        borderRadius: 10,
+        borderWidth: 1,
+        // marginLeft: 62,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // alignSelf: 'center',
+        paddingTop: 10,
+        width: 251,
+        height: 37,
+        paddingLeft: 14,
+        fontFamily: 'Segoe UI',
+        fontSize: 13,
+        color: '#808080',
+    }
+})
