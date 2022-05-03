21692774e1dac2b843e93abc3186ff7b738a20d7
Erick <S21910250@student.unklab.ac.id>
2022-05-03 18:27:38 +0800

    fix conflict

--- project_final/App.js
+++ project_final/App.js
@@ -1,42 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {
-  DetailsF_BarDesc,
-  DetailsF_Header,
-  DetailsU_Bar,
-  DetailsU_Header,
-  Footer,
-  HeaderUser,
-  SignUpF_Button,
-  SignUpF_Footer,
-  SignUpF_Header,
-  SignUpF_TextInput,
-  DetailsF_BarSkill,
-  DetailsF_BarClient,
-  DetailsF_BarEmail,
-  DetailsF_Content,
-} from './src/components';
-import {
-  DetailsF,
-  HomeF,
-  SelectUser,
-  ProfileF,
-  ProfileC,
-  SearchJob,
-} from './src';
-import BorderUser from './src/components/SelectUser_border';
-import SplashScreen from './src/pages/SplashScreen';
-import HomeF_Header from './src/components/HomeF_Header';
+
 const App = () => {
   return (
     <View>
-      {/* <SelectUser/> */}
-      {/* <SplashScreen/> */}
-      {/* <HomeF_Header/> */}
-      {/* <HomeF/> */}
-      {/* <ProfileF /> */}
-      {/* <ProfileC /> */}
-      <SearchJob />
+      <Text>App</Text>
     </View>
   );
 };
