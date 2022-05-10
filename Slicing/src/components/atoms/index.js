7b7b77bb6987dfbb5905a963d3ac7c9718c18697
Jeremia Waani <s21910028@student.unklab.ac.id>
2022-05-11 07:12:19 +0800

    tes

--- Slicing/src/components/atoms/index.js
+++ Slicing/src/components/atoms/index.js
@@ -2,6 +2,5 @@ import Button from './Button';
 import TextInput from './TextInput';
 import Gap from './Gap';
 import Label from './Label';
-import ListTransaction from './ListTransaction';
 
-export {Button, TextInput, Gap, Label, ListTransaction};
+export {Button, TextInput, Gap, Label};
